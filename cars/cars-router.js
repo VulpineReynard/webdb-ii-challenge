f340e6ee64a1441fb3b3d7bb9461a3213515492a
VulpineReynard <milare8@gmail.com>
2019-09-10 17:12:20 -0400

    adding cars functionality works

--- cars/cars-router.js
+++ cars/cars-router.js
@@ -14,5 +14,15 @@ router.route("/")
       res.status(500).json({ message: 'Failed to retrieve cars.' })
     })
 })
+.post(function rootPostController(req, res) {
+  const newCarData = req.body;
+  db('cars').insert(newCarData)
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json({ message: "Couldn't create car." })
+    })
+})
 
 module.exports = router;
